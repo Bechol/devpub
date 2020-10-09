f5eed31ae845291c7d3cb6c62c6dc054c3b14e57
Oleg Bech <oleg071984@gmail.com>
2020-10-09 11:47:19 +0300

    init commit

---
+++ src/main/java/ru/bechol/devpub/DevpubApplication.java
@@ -0,0 +1,13 @@
+package ru.bechol.devpub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevpubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevpubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bechol/devpub/DevpubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bechol.devpub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevpubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
