cf231f225677189f146354c81db68ba2ef03846f
Oleg Bech <oleg071984@gmail.com>
2020-10-11 15:28:00 +0300

    GET /api/auth/registrate test

--- src/test/java/ru/bechol/devpub/controller/ApiAuthControllerTest.java
+++ src/test/java/ru/bechol/devpub/controller/ApiAuthControllerTest.java
@@ -1,18 +1,23 @@
 package ru.bechol.devpub.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import ru.bechol.devpub.request.RegisterRequest;
 import ru.bechol.devpub.response.CaptchaResponse;
 import ru.bechol.devpub.service.CaptchaCodesService;
+import ru.bechol.devpub.service.UserService;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,6 +28,8 @@ public class ApiAuthControllerTest extends AbstractControllerTest {
     private ApiAuthController apiAuthController;
     @MockBean
     private CaptchaCodesService captchaCodesService;
+    @MockBean
+    private UserService userService;
 
     @Test
     public void apiAuthControllerContextLoads() {
@@ -40,6 +47,24 @@ public class ApiAuthControllerTest extends AbstractControllerTest {
                 .andExpect(content().string(containsString(captchaResponse.getCode())))
                 .andExpect(content().string(containsString(captchaResponse.getSecret())))
                 .andReturn();
+    }
+
+    @Test
+    public void whenRegistrationFormDataInvalidThenGetResponseWithErrors() throws Exception {
+        RegisterRequest registerRequest = new RegisterRequest();
+        registerRequest.setE_mail("mail.mail.ru");
+        registerRequest.setName("");
+        registerRequest.setCaptcha("asdb3n");
+        registerRequest.setCaptcha_secret(UUID.randomUUID().toString());
+        registerRequest.setPassword("pass");
+        ObjectMapper objectMapper = new ObjectMapper();
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(registerRequest)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
     }
 
 
