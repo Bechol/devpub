669a21e802ffd4f16170e4f239e97b49360ac1f6
Oleg Bech <oleg071984@gmail.com>
2020-10-11 19:20:40 +0300

    added messages bundle

---
+++ src/main/java/ru/bechol/devpub/configuration/MessagesConfig.java
@@ -0,0 +1,16 @@
+package ru.bechol.devpub.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class MessagesConfig {
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasenames("messages");
+        return resourceBundleMessageSource;
+    }
+}
