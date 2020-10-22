b50e9ce84782425d4abdebb3d7f58678a16b8895
Oleg Bech <oleg071984@gmail.com>
2020-10-23 01:31:41 +0300

    Fix tests

--- src/test/java/ru/bechol/devpub/controller/DefaultControllerTest.java
+++ src/test/java/ru/bechol/devpub/controller/DefaultControllerTest.java
@@ -1,6 +1,6 @@
 package ru.bechol.devpub.controller;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
