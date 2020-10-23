44611dc94d904a78ec5d120cea20dd28ee4b83a0
Oleg Bech <oleg071984@gmail.com>
2020-10-24 02:53:40 +0300

    POST /api/auth/restore

---
+++ src/main/java/ru/bechol/devpub/request/EmailRequest.java
@@ -0,0 +1,15 @@
+package ru.bechol.devpub.request;
+
+import lombok.Data;
+
+/**
+ * Класс EmailRequest.
+ * Ввод email пользователем на форме.
+ * @author Oleg Bech
+ * @email oleg071984@gmail.com
+ */
+@Data
+public class EmailRequest {
+
+    private String email;
+}
