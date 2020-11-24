cab9f1742067d183aadf5c2cc9d5889edf0cf1b8
Oleg Bech <oleg071984@gmail.com>
2020-11-25 01:55:17 +0300

    Add role service.

---
+++ src/main/java/ru/bechol/devpub/service/RoleService.java
@@ -0,0 +1,41 @@
+package ru.bechol.devpub.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.bechol.devpub.models.Role;
+import ru.bechol.devpub.repository.RoleRepository;
+
+import javax.management.relation.RoleNotFoundException;
+
+/**
+ * Класс RoleService.
+ * Сервсиный слой для Role.
+ *
+ * @author Oleg Bech
+ * @email oleg071984@gmail.com
+ * @see Role
+ * @see RoleRepository
+ * @see Messages
+ */
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private Messages messages;
+
+    /**
+     * Метод findByName.
+     * Поиск роли по наименованию.
+     * @param roleName - наименование роли.
+     * @return Role
+     * @throws RoleNotFoundException - в случае если роль не найдена по наименованию
+     */
+    public Role findByName(String roleName) throws RoleNotFoundException {
+        return roleRepository.findByName(roleName)
+                .orElseThrow(() -> new RoleNotFoundException(
+                        messages.getMessage("role-not-found.exception.message", roleName)
+                ));
+    }
+}
